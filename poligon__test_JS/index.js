910358dfdb3db8cc9c2a2441189fd7069ece0024
Alexander <117478092+NanoBabaika@users.noreply.github.com>
2023-12-23 12:37:36 +0300

    Add files via upload

--- poligon__test_JS/index.js
+++ poligon__test_JS/index.js
@@ -1,35 +1,68 @@
 
-// function summ (a,b, ...numbers) {
-//     let result =  a + b;
-
-//     for(let i= 0;  i < numbers.length; i++){
-//         result = result + numbers[i];
-//     }
-
-//     return result;
-// }
-
-// console.log(summ(1,2, 3,4,5));
-
-
-
 // Задача 1
 function returnArgument (){
     return argument;  
 }
 
-let argument = 'Hi!';
- 
+var argument = 'Hi!';
 console.log(returnArgument(argument));
- 
+var argument = 100;
+console.log(returnArgument(argument));
+
 
 // Задача2
 function summ(a, b){
-    result = a +b ;
+    result = a + b;
     return result;
 }
 
-// var a = 15;
-// var b =25;
-
 console.log(summ(15, 25));
+
+
+// Задача 2.2
+function summ22 (arguments, d = 100){
+    result = arguments + d;
+    return result;
+}
+
+
+console.log(summ22(10));
+
+
+
+// Задача 3
+function resultReturn (funcHi) {
+    function funcHi () {
+        return 'Hi from funcHi!';
+    }
+     return funcHi();
+}
+ 
+
+
+// Задача 4 
+function returnCounter(){
+    let f = 10;
+
+    function inc () {
+        f += 1;
+        return f;
+    }
+
+    return inc;
+}
+
+const counter = returnCounter();
+console.log(counter());
+console.log(counter());
+console.log(counter());
+console.log(counter());
+
+
+
+// Задача 5 
+function createMassive (...arguments) {
+    return Array.of(...arguments);;
+}
+
+console.log(createMassive(1, 'Hi from array!', 20, 11));
