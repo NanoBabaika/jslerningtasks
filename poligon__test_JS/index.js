867d2d0b74514108ee6ebaa33cb927d049ae7fdb
Alexander <117478092+NanoBabaika@users.noreply.github.com>
2023-12-23 12:25:07 +0300

    Add files via upload

---
+++ poligon__test_JS/index.js
@@ -0,0 +1,35 @@
+
+// function summ (a,b, ...numbers) {
+//     let result =  a + b;
+
+//     for(let i= 0;  i < numbers.length; i++){
+//         result = result + numbers[i];
+//     }
+
+//     return result;
+// }
+
+// console.log(summ(1,2, 3,4,5));
+
+
+
+// Задача 1
+function returnArgument (){
+    return argument;  
+}
+
+let argument = 'Hi!';
+ 
+console.log(returnArgument(argument));
+ 
+
+// Задача2
+function summ(a, b){
+    result = a +b ;
+    return result;
+}
+
+// var a = 15;
+// var b =25;
+
+console.log(summ(15, 25));
