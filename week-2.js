5a94f3fe56f9c757ebdcd8663042b6694440b5cd
Alexandr Skoromets <skoromets1990@mail.ru>
2023-12-26 17:14:17 +0300

    Test commit

--- week-2.js
+++ week-2.js
@@ -1,26 +1,15 @@
-// const student = {
-//     name:'Василий',
-//     lastName:'Петров'
-// }
+let summ = (a, b =10) => a + b;
 
+console.log(summ(10));
 
-// const {name:firstName, lastName, age = 22} = student;
-
-// console.log(`${firstName} ${lastName}, (${age})`);
-
-
-// let summ = (a, b =10) => a + b;
-
-// console.log(summ(10));
-
-const numbers = [1, 2, 3, 4];
+const arr1 = [1, 2, 3, 4];
 
 function wievMassive (...args) {
     console.log(...args)
 }
 
 
-wievMassive (...numbers);
+wievMassive (...arr1);
 
 
 
@@ -36,3 +25,9 @@ arr.forEach(function(item, i, arr) {
     console.log( i + ": " + item + " (массив:" + arr + ")" );
 });
 
+
+const numbers = [1, 2, 3, 4, 5]; 
+
+for (let i = 0; i < numbers.length; i++) {
+    console.log(numbers[i]);
+}
