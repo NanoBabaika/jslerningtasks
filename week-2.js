07f92a51bea7b10ac31d711f366feb7bfa3a8498
[Skoromets Alexandr] <[skoromets1990@mail.ru]>
2023-12-31 00:00:14 +0300

    task 5

--- week-2.js
+++ week-2.js
@@ -20,5 +20,24 @@ keys.forEach(function (item, i, keys) {
 
 
   // Задача 5
- isAllTrue([1, 2, 3, 4, 5], n => n < 10) // вернет true (потому что все элементы массива меньше 10)
- isAllTrue([100, 2, 3, 4, 5], n => n < 10) // вернет false (потому что как минимум первый элемент больше 10)
+const numbers = [1, 2, 3, 4, 20];
+
+function fn (numbers, value){
+  for(var i = 0; i < 10; i++) {
+    if (i < 10) {
+      return true
+    } else {
+      return false
+    }
+  }
+ }
+
+console.log(fn());
+
+// isAllTrue(numbers, fn) {
+
+// }
+
+
+//  isAllTrue([1, 2, 3, 4, 5], n => n < 10) // вернет true (потому что все элементы массива меньше 10)
+//  isAllTrue([100, 2, 3, 4, 5], n => n < 10) // вернет false (потому что как минимум первый элемент больше 10)
