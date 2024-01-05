dc6d581dda98ed182eb67f595908830a2653c4ee
[Skoromets Alexandr] <[skoromets1990@mail.ru]>
2024-01-05 23:32:04 +0300

    New file Tasks

--- week-2.js
+++ week-2.js
@@ -33,6 +33,87 @@ const numbers = [1, 2, 3, 4, 20];
   }
  })
  
-  // Задача 5.2
+  // Задача 6
+  // isSomeTrue([1, 2, 30, 4, 5], n => n > 20) // вернет true (потому что в массиве есть хотя бы один элемент больше 20)
+  // isSomeTrue([1, 2, 3, 4, 5], n => n > 20) // вернет false (потому что в массиве нет ни одного элемента больше 20)
+
+  // const numbers2= [1, 2, 30, 4, 5];
 
   
+
+
+ 
+ 
+// Терория
+const arr2 = [1, 2, 3, 4, 5]; 
+
+const arr3 = ['new element', 'new element2'];
+
+arr2.splice(3, 0, ...arr3);
+
+console.log(arr2);
+ 
+
+// копирование массива
+const arr4 = [1, 2, 3, 4, 5];
+
+const arr5 = [...arr4];
+
+arr5[2] = 'Hi from arr5!';
+
+console.log(arr4);
+console.log(arr5);
+
+// Перебор массива
+
+for ( let i = 0; i<arr5.length; i++) {
+  console.log(arr5[i]);
+}
+
+
+
+const krista = {
+  name: 'Kristina',
+  LastName:'Evseeva',
+  age: 32
+}
+
+// так можно вывести ключи объекта что бы к ним потом обратиться
+// console.log(Object.keys(krista));
+// console.log(Object.values(krista));
+// console.log(Object.entries(krista));
+
+for(const values of Object.values(krista)) {
+  console.log(values)
+}
+
+
+
+const student1 = {
+  name: 'Vasiliy',
+  LastName:'Petrov'
+}
+
+student1.age = 31;
+
+
+console.log(student1.name, student1.age);
+
+// строковая интерполяция
+
+// console.log(`${student1.name} ${student1.LastName}, (${student1.age})`);
+
+// Деструктурирующее присваивание объекты
+
+const {name, LastName, age} = student1
+
+console.log(`${name} ${LastName}, (${age})`);
+
+b = 15
+
+function sum ( a = 10) {
+   result = a + b;
+   return result
+}
+
+console.log(sum());
